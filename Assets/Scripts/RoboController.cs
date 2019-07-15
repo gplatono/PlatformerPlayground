b5ed318744a43fe6cc2612828d94a6611be56eb6
gplatono <gr.plat@gmail.com>
2019-07-15 00:17:50 -0400

    first commit

---
+++ Assets/Scripts/RoboController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float horizontal = Input.GetAxis("Horizontal");
+    	float vertical = Input.GetAxis("Vertical");
+      	Debug.Log(horizontal);
+        Vector2 position = transform.position;
+        position.x += 3.0f * horizontal * Time.deltaTime;
+        position.y += 3.0f * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
